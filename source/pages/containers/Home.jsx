d28de0cd04a18678ffcc5d3a5608b333c7696cfe
luis mori <luismori@MacBook-Pro-de-luis.local>
2017-01-28 18:14:49 -0500

    scroll part 1

--- source/pages/containers/Home.jsx
+++ source/pages/containers/Home.jsx
@@ -15,6 +15,8 @@ class Home extends Component {
             posts: [],
             loading: true
         };
+
+        this.handleScroll = this.handleScroll.bind(this);
     }
 
     async componentDidMount() {
@@ -25,19 +27,53 @@ class Home extends Component {
             page: this.state.page + 1,
             loading: false
         })
+
+        window.addEventListener('scroll', this.handleScroll);
+    }
+    componentWillUnmount () {
+        window.removeEventListener('scroll', this.handleScroll);
+    }
+
+
+    handleScroll(event) {
+        if (this.state.loading) return null;
+
+        console.log("Scrolling");
+        console.log(this.state.page);
+
+        const scrolled = window.scrollY;
+        const viewportHeight = window.innerHeight;
+        const fullHeight = document.body.clientHeight;
+
+        if (!(scrolled + viewportHeight + 200 >= fullHeight)) return null;
+
+        this.setState({ loading: true }, async () => {
+            try {
+                const posts = await api.posts.getList(this.state.page);
+
+                this.setState({
+                    posts: this.state.posts.concat(posts),
+                    page: this.stage.page + 1,
+                    loading: false
+                });
+
+            } catch(error) {
+                console.log(error);
+                this.setState({loading: false})
+            }
+        })
     }
     
-
+    
     render() {
 
         return (
             <section name="Home">
-                <section>
-                    {this.state.loading && (
-                        <Loading />)}
-                    {this.state.posts.map(post => <Post key={post.id} {...post} />)}
-                </section>
                 <h1>Home</h1>
+                <section>
+                    {this.state.posts.map(post => <Post key={post.id} {...post} />)}
+                     {this.state.loading && (<Loading />)}
+                </section>
             
              </section>
         );
