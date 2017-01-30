830c45cf3a1874bc17872ee3cd05d8791c978c78
luis mori <luismori@MacBook-Pro-de-luis.local>
2017-01-29 20:01:10 -0500

    middlewares

--- source/store.js
+++ source/store.js
@@ -1,6 +1,31 @@
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import reducer from './reducer';
-const store = createStore(reducer)
+
+
+// const logger (store) => {
+//     return (next) => {
+//         return (action) => {
+            
+//         }
+//     }
+// }
+
+const logger = store => next => action => {
+    console.group('logger')
+    console.debug("stado actual", store.getState());
+    console.debug('accion', action);
+    const result = next(action);
+    console.debug("stado nuevo", store.getState());
+    console.groupEnd('logger')
+    
+    return result;
+}
+
+const store = createStore(
+    reducer, 
+    applyMiddleware(
+        logger,
+    ))
 
 
 export default store;
